2cea417d1b8667eb61ea6fe3551a11fa30d9506f
ashitakalax <leviballing@gmail.com>
2021-07-13 21:46:28 -0600

    Prototyped with some spare LED's

---
+++ midnight-wee-light.ino
@@ -0,0 +1,36 @@
+// Author: Levi Balling
+// Application Name: Midnight-Wee
+// Description: a light that will slowely come on to
+
+#include "FastLED.h"
+#define NUM_LEDS 60
+// Max is from 0-255
+#define MAX_RED_BRIGHTNESS 25
+#define LED_DATA_PIN 6
+#define TIME_BETWEEN_INCREMENTS 75
+CRGB leds[NUM_LEDS];
+
+
+bool lightOn;
+void setup() {
+  // put your setup code here, to run once:
+  FastLED.addLeds<NEOPIXEL, 6>(leds, NUM_LEDS); 
+  fill_solid( &(leds[0]), NUM_LEDS, CRGB( 0, 0, 0) );
+
+  lightOn = false;
+}
+
+void loop() {
+  while(lightOn)
+  {
+    delay(1000);
+  }
+  for(int brightness = 0; brightness < MAX_RED_BRIGHTNESS; brightness++) 
+  { 
+    fill_solid( &(leds[0]), NUM_LEDS, CRGB( brightness, 0, 0) );
+    FastLED.show();
+    // clear this led for the next time around the loop
+    delay(TIME_BETWEEN_INCREMENTS);
+  }
+   lightOn = true;        
+}
